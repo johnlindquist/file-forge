49d178d79cf79ef748878d5b33f06be72292b73a
johnlindquist <johnlindquist@gmail.com>
2025-01-27 17:18:28 -0700

    fix(eslint): allow node globals and enforce no unused vars

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js"
 import ts from "@typescript-eslint/eslint-plugin"
 import tsParser from "@typescript-eslint/parser"
+import globals from "globals"
 
 export default [
   js.configs.recommended,
@@ -12,12 +13,16 @@ export default [
         ecmaVersion: "latest",
         sourceType: "module",
       },
+      globals: {
+        ...globals.node,
+      },
     },
     plugins: {
       "@typescript-eslint": ts,
     },
     rules: {
       ...ts.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": "error",
     },
   },
 ] 
